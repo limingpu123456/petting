dee785312e4328bf90a7d92ab05cc8bce19db657
limingpu <85180586@qq.com>
2023-11-14 23:14:29 +0800

    fix

--- petting-frontend/src/network/requestnotice.js
+++
@@ -1,9 +0,0 @@
-import request from "network/request.js"
-
-//查找聊天记录
-export function requestqueryusernotice(parameter) {
-    return request({
-        url: "/notice/queryusernotice",
-        params: parameter
-    })
-}
